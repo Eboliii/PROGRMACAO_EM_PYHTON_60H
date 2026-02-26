c89007dedf0865785b039cb0dad3f337f789ab62
Eboliii <r.eboli.ben@gmail.com>
2026-02-26 17:25:56 -0300

    Adicionado uma Pasta"Linguagem em Python"
    Conteúdo da pasta é uma arquivo python sobre a aula do dia 26/02/2026 contendo algumas informações sobre dados primitivos

---
+++ Linguagem Python/linguagem_python.py
@@ -0,0 +1,24 @@
+#Aula de programação em python 26/02
+print('Olá Mundo...')
+
+#dados_primitivos_em_python
+
+#tipo_string_textos
+'teste 1'
+"teste 2"
+
+#tipo_interger_inteiros
+1
+2
+100
+
+#tipo_float_real
+5.0
+0.5
+1.
+0.
+3.3
+
+#tipo_bool_logico
+True
+False
