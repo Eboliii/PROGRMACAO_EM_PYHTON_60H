33a9a6a24a72c89fb0d9a0a6ef12e21d9feaf397
Eboliii <r.eboli.ben@gmail.com>
2026-02-27 16:47:17 -0300

    Adicionado um arquivo
    Adicionado o arquivo "atividades.py" foi adicionado contendo as atividades realizadas no dia 27/02/2026

---
+++ Linguagem Python/atividades.py
@@ -0,0 +1,24 @@
+print('Atividade 1')
+n1 = 10
+print('O número é: ',n1,'e o seu valor ao quadrado é: ',n1**2)
+print('***'*15)
+print('Atividade 2')
+nome = "Rafael"
+sobrenome = "Eboli Benaglia"
+print('O nome do Aluno é: ',nome,sobrenome)
+print('***'*15)
+print('Atividade 3')
+print('Digite os Números a Seguir')
+numero1 = input('Digite o primeiro número: ')
+numero2 = input('Digite o segundo número: ')
+print('Os números digitados foram: ',numero1,' e ',numero2)
+print('***'*15)
+print('Atividade 4')
+py = 'Python'
+print('O programa ',py,'esta na versão',3.14)
+print('***'*15)
+print('Atividade 5')
+salario = 1500
+nome2 = input('Digite seu nome inteiro: ')
+print('O sálario do fúncionario',nome2,'é de ',salario)
+print('***'*15)
