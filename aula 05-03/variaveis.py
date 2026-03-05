4ceb1477cb48cbbde5574b990d381570ebff35c1
Eboliii <r.eboli.ben@gmail.com>
2026-03-05 15:59:12 -0300

    Add files via upload

---
+++ aula 05-03/variaveis.py
@@ -0,0 +1,67 @@
+# metodos - funções
+
+#PALAVRA() -> Função - Ação
+
+#exemplos
+#print()
+#input()
+
+palavra = 'variavel'
+variavel = 10
+listas = [10, 2, 3]
+tuplas = (19, 8, 0)
+conjuntos = {1 ,2 ,3}
+dicionario = {'key', 'value'}
+
+
+#estrutura de dados - objetos
+
+#Funções Casting
+#str()
+#float()
+#int()
+#bool()
+
+#len()
+#max()
+#min()
+
+#list()
+
+#tuple()
+
+#sorted()
+#sum()
+
+#_________________________________________________________________________
+
+#objeto = 'texto'
+
+#print(len(objeto)) #tamanho
+
+#x = (max(objeto))
+#print (x)
+#print (min(objeto))
+
+#text = 'isso é um texto'
+
+#print(list(text))
+#print(tuple(text))
+
+#x = sorted(text)
+
+#print(x)
+#text.sort()
+#print(dir(x))
+
+#x = 15
+#y = 10
+
+#print(sum([1, 2, 3]))
+
+r = list(range(11))
+print(r)
+
+
+
+
