2b658a532f03cf33b6893756e235ff5397036f47
Eboliii <r.eboli.ben@gmail.com>
2026-03-05 17:52:56 -0300

    Add files via upload

---
+++ aula 05-03/listas.py
@@ -0,0 +1,29 @@
+#lista_mista =  [1,2,'texto',False,5.6]
+#lista_inteira = [1,2,3]
+#lista_decimal = [0.1,0.3,1.9,]
+#lista_texto = ['texto1','texto2','texto3']
+#lista_boleana = [False, True, True]
+#l = list(range(1, 7))
+#print(l)
+
+numeros = list(range(1,11))
+print(f'''
+    Resultado
+    {numeros}
+''')
+#____________________________________________
+print(f'''
+    {numeros[2]}
+''')
+#____________________________________________
+numeros.append(9)
+print(numeros)
+#____________________________________________
+del numeros[4]
+print(numeros)
+#____________________________________________
+carros = ['Silvia S11','Silvia S12','Silvia S13']
+print(f'''
+    {numeros}
+    {carros}
+''')
